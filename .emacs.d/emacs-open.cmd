7e39535fafb8adc133cec96629e455a351be81ad
Ted de Munnik <tedm@eidos.com>
2016-07-21 09:55:17 +0200

    Added emacs server, and command to open files with emacs

---
+++ .emacs.d/emacs-open.cmd
@@ -0,0 +1,1 @@
+emacsclientw -na runemacs %1 --server-file="%HOME%\.emacs.d\server\server"
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,17 @@
-; Package system initialisation
+;; Package system initialisation
 (add-to-list 'load-path "~/.emacs.d/lib/")
 (require 'package)
 (eval-when-compile (require 'use-package))
 (add-to-list 'package-archives `("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
-; UI Customisation 
+;; Emacs Server
+;; By running this ensures that only a single instance of emacs is active, and trying to open a second one
+;; will instead open the file in the existing emacs instance.
+(require 'server)
+(unless (server-running-p) (server-start))
+
+;; UI Customisation 
 (use-package monokai-theme
   :ensure t
   :init (load-theme 'monokai t))
@@ -15,21 +21,29 @@
 (tool-bar-mode -1) ; No toolbar in UI emacs
 (scroll-bar-mode -1) ; No scrollbar in UI emacs
 
-; Evil for dat vim emulation
+;; Evil for dat vim emulation
 (use-package evil
+  :ensure t
   :init
-  (evil-mode 1))
+  (evil-mode 1)
+  )
 
-; Combination of helm/projectile to get Ctrl+P find files in project
+;; Combination of helm/projectile to get Ctrl+P find files in project
 (use-package helm
   :ensure t)
+
 (use-package projectile
   :ensure t
   :config
   (use-package helm-projectile
     :ensure t
     :init
-    (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile-find-file)))
+    (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile-find-file))
   :init
   (projectile-global-mode)
+  (setq projectile-indexing-method 'alien))
 
+(use-package p4
+  :ensure t
+  :init
+  (evil-ex-define-cmd "P4edit" `p4-edit))
